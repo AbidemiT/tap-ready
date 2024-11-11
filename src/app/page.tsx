9d283c25544f68d6c55d454b151e13b36a040f90
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2024-11-11 11:07:13 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,157 +1,243 @@
 "use client";
-
-import { useEffect, useState } from "react";
-import {
-  motion,
-  useAnimate,
-  useTransform,
-  useMotionValue,
-} from "framer-motion";
+import { motion, useMotionValue, useAnimate, AnimationSequence, easeIn } from "framer-motion";
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { useMemo, useEffect } from "react";
 // import GridLayout from "@/components/GridLayout";
 
 export default function Home() {
-  const firstView = useMotionValue(0);
-  const secondView = useMotionValue(0);
-  const thirdView = useMotionValue(0);
-  const fourthView = useMotionValue(0);
-
-  const firstViewHeight = useTransform(
-    firstView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const secondViewHeight = useTransform(
-    secondView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const thirdViewHeight = useTransform(
-    thirdView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const fourthViewHeight = useTransform(
-    fourthView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const firstViewRadius = useTransform(firstView, [1, 2, 3, 4, 5, 6], ["50%", "24px", "50%", "24px", "50%", "24px"]);
-  const secondViewRadius = useTransform(secondView, [1, 2,3,4,5,6], ["50%", "24px", "50%", "24px", "50%", "24px"]);
-  const thirdViewRadius = useTransform(thirdView, [1, 2, 3, 4, 5, 6], ["50%", "24px", "50%", "24px", "50%", "24px"]);
-  const fourthViewRadius = useTransform(fourthView, [1, 2,3,4,5,6], ["50%", "24px", "50%", "24px", "50%", "24px"]);
-  const firstViewWidth = useTransform(
-    firstView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const secondViewWidth = useTransform(secondView, [1, 2], ["100%", "100%"]);
-  const thirdViewWidth = useTransform(
-    thirdView,
-    [1, 2, 3, 4, 5, 6],
-    ["100%", "100%", "100%", "100%", "100%", "100%"]
-  );
-  const fourthViewWidth = useTransform(fourthView, [1, 2], ["100%", "100%"]);
-
-  const morphViewsClass = [
-    "firstView",
-    "secondView",
-    "thirdView",
-    "fourthView",
-  ];
-
+  // const progress = useMotionValue(0);
   const [scope, animate] = useAnimate();
-
-  const [currentView, setCurrentView] = useState(morphViewsClass[0]);
+  // const sequence: AnimationSequence = useMemo(() => [
+  //   ['li.masonryItem-1', { height: 84, width: "100%", borderRadius: "48px", gridArea: "2 / 1 / span 1 / span 3" }, { duration: 1 }],
+  //   [progress, 100, { ease: "easeInOut", duration: 1 }]
+  // ], [progress]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const currentIndex = morphViewsClass.indexOf(currentView);
-      const nextIndex = (currentIndex + 1) % morphViewsClass.length;
-      setCurrentView(morphViewsClass[nextIndex]);
-    }, 2000);
-    
-    return () => clearInterval(interval);
-  }, [currentView]);
+    handleMansoryItemOne();
+  }, []);
 
-  const getHeight = () => {
-    switch (currentView) {
-      case "firstView":
-        return firstViewHeight;
-      case "secondView":
-        return secondViewHeight;
-      case "thirdView":
-        return thirdViewHeight;
-      case "fourthView":
-        return fourthViewHeight;
-      default:
-        return firstViewHeight;
-    }
-  };
-  const getWidth = () => {
-    switch (currentView) {
-      case "firstView":
-        return firstViewWidth;
-      case "secondView":
-        return secondViewWidth;
-      case "thirdView":
-        return thirdViewWidth;
-      case "fourthView":
-        return fourthViewWidth;
-      default:
-        return firstViewWidth;
-    }
-  };
-  const getRadius = () => {
-    switch (currentView) {
-      case "firstView":
-        return firstViewRadius;
-      case "secondView":
-        return secondViewRadius;
-      case "thirdView":
-        return thirdViewRadius;
-      case "fourthView":
-        return fourthViewRadius;
-      default:
-        return firstViewRadius;
+  const handleMansoryItemOne = async () => {
+    try {
+      await animate("li.masonryItem-1", {y: "-40%"});
+
+      await animate("li.masonryItem-1", {y: "0%"});
+
+      await animate("li.masonryItem-1", {y: "40%"});
+
+
+    } catch (error) {
+      console.log(error);
+      
     }
   };
 
   return (
     <div>
-      {/* <h1>Hello</h1> */}
       <div className="masonry-wrapper">
-        <ul className={styles.masonry} ref={scope}>
-          {[...Array(6)].map((_, index) => (
+        <motion.ul className={styles.masonry} ref={scope} >
             <motion.li
-              key={index}
-              className={`${styles.masonryItem} 
-              ${styles[`${currentView}-${index + 1}`]}`}
-              style={{
-              overflow: "hidden",
-              height: getHeight(),
-              width: getWidth(),
-              borderRadius: getRadius(),
+              className={`${styles.masonryItem} ${styles.masonryItem}-1`}
+              initial={{
+                height: "84px",
+                width: "84px",
+                borderRadius: "50%",
+                gridColumn: 1,
+                gridRow: 1,
               }}
               animate={{
-              height: getHeight(),
-              width: getWidth(),
-              borderRadius: getRadius(),
+                height: [ "84px", "64px", "84px"],
+                width: [ "300px", "64px", "84px"],
+                borderRadius: [ "48px", "32px", "50%"],
+                gridColumn: [ "1 / span 3", "1 / span 1", "1 / span 1"],
+                gridRow: [ "2 / span 1", "1 / span 1", "1 / span 1"],
+                // gridArea: ["1 / 1 / span 1 / span 1", "2 / 1 / span 1 / span 3", "1 / 1 / span 1 / span 1", "1 / 1 / span 1 / span 1"],
               }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 5, ease: "easeInOut", repeat: Infinity }}
             >
               <Image
-              src={`/${index + 1}.webp`}
+                src={`/1.webp`}
+                width={100}
+                height={100}
+                alt=""
+                className="object-cover w-full h-full"
+                priority
+              />
+            </motion.li>
+           <motion.li
+            className={`${styles.masonryItem} ${styles.masonryItem}-2`}
+            initial={{ height: "100%", width: "64px", borderRadius: "48px", gridArea: "1 / 2 / span 3 / span 1" }}
+            animate={ {
+              height: [ "64px", "84px", "100%"],
+              width: [ "64px", "64px", "64px"],
+              borderRadius: [ "50%", "32px", "48px"],
+              gridArea: [ "1 / 2 / span 1 / span 1", "1 / 2 / span 1 / span 1", "1 / 2 / span 3 / span 1"],
+              // x: ["33%", "33%", "33%", "33%"],
+            }
+            // ,[
+            //   {
+            //     height: 64,
+            //     width: 64,
+            //     borderRadius: "50%",
+            //     gridColumn: 2,
+            //     gridRow: 1,
+            //   },
+            //   {
+            //     height: 84,
+            //     width: 64,
+            //     borderRadius: "32px",
+            //     gridColumn: 2,
+            //     gridRow: 1,
+            //   },
+            //   {height: "100%", width: "64px", borderRadius: "48px", gridArea: "1 / 2 / span 3 / span 1" },
+            // ]
+          }
+          transition={{ duration: 5, ease: "easeInOut", repeat: Infinity }}
+          >
+            <Image
+              src={`/2.webp`}
               width={100}
               height={100}
               alt=""
               className="object-cover w-full h-full"
               priority
-              />
-            </motion.li>
-          ))}
-        </ul>
+            />
+          </motion.li>
+          {/*
+          <motion.li
+            className={`${styles.masonryItem} ${styles.masonryItem}-3`}
+            initial={{ borderRadius: "48px", gridRow: "span 3" }}
+            animate={[
+              {
+                height: 72,
+                width: 84,
+                borderRadius: "32px",
+                gridColumn: 3,
+                gridRow: 1,
+              },
+              {
+                height: 64,
+                width: "100%",
+                borderRadius: "32px",
+                gridArea: "2 / 2 / span 2 / span 3",
+              },
+              { height: "100%", width: "64px", borderRadius: "48px", gridArea: "1 / 3 / span 3 / span 1" },
+            ]}
+            transition={{ duration: 30, repeat: Infinity }}
+          >
+            <Image
+              src={`/3.webp`}
+              width={100}
+              height={100}
+              alt=""
+              className="object-cover w-full h-full"
+              priority
+            />
+          </motion.li>
+          <motion.li
+            className={`${styles.masonryItem} ${styles.masonryItem}-4`}
+            initial={{ borderRadius: "48px", gridRow: "span 2" }}
+            animate={[
+              {
+                height: 72,
+                width: 72,
+                borderRadius: "50%",
+                gridColumn: 1,
+                gridRow: 3,
+              },
+              { gridColumn: 1, gridRow: 2 },
+              {height: "100%", width: "72px", borderRadius: "48px", gridRow: "2 / span 2" },
+            ]}
+            transition={{ duration: 30, repeat: Infinity }}
+          >
+            <Image
+              src={`/4.webp`}
+              width={100}
+              height={100}
+              alt=""
+              className="object-cover w-full h-full"
+              priority
+            />
+          </motion.li>
+          <motion.li
+            className={`${styles.masonryItem} ${styles.masonryItem}-5`}
+            initial={{
+              height: 84,
+              width: 84,
+              borderRadius: "50%",
+              gridColumn: 1,
+              gridRow: 4,
+              alignSelf: "center",
+            }}
+            animate={[
+              { height: 72, width: 72, borderRadius: "50%" },
+              {
+                height: "100%",
+                width: "100%",
+                borderRadius: "56px",
+                gridArea: "3 / 1 / span 2 / span 2",
+              },
+              {
+                height: 84,
+                width: 84,
+                borderRadius: "50%",
+                gridArea: "4 / 1 / span 1 / span 1"
+              },
+            ]}
+            transition={{ duration: 30, repeat: Infinity }}
+          >
+            <Image
+              src={`/5.webp`}
+              width={100}
+              height={100}
+              alt=""
+              className="object-cover w-full h-full"
+              priority
+            />
+          </motion.li>
+          <motion.li
+            className={`${styles.masonryItem} ${styles.masonryItem}-6`}
+            initial={{
+              height: "100%",
+              width: "100%",
+              borderRadius: "56px",
+              gridColumn: "2 / span 2",
+              gridRow: "4 / span 3",
+            }}
+            animate={[
+              {
+                borderRadius: "48px",
+                gridColumn: "2 / span 2",
+                gridRow: "3 / span 2",
+              },
+              {
+                height: 72,
+                width: 72,
+                borderRadius: "50%",
+                gridColumn: 3,
+                gridRow: 4,
+              },
+              {
+                height: "100%",
+                width: "100%",
+                borderRadius: "56px",
+                gridColumn: "2 / span 2",
+                gridRow: "4 / span 3",
+                alignSelf: "flex-start",
+              },
+            ]}
+            transition={{ duration: 30, repeat: Infinity }}
+          >
+            <Image
+              src={`/6.webp`}
+              width={100}
+              height={100}
+              alt=""
+              className="object-cover w-full h-full"
+              priority
+            />
+          </motion.li> */}
+        </motion.ul>
       </div>
     </div>
 
